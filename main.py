7ec3c6004f35afaff3efcc063c4f1f04fd47d363
719Ben <ben.williams@colorado.edu>
2016-08-06 22:21:13 -0700

    Split callback

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import requests
 import sys
 
 app_key = "ETy&^^ReC*R+Ynk*GD6YQVmyf!3TdYWS"
+page_access_token = "EAAYpzmsUt2UBABvSyMBgPqriDAWjlV49k7ZC9doivITdKKlgx27GGz76Vd7y8EZCLllZAlEyoRu4g9zJncUdsZAvXW6BHUl2X84ZCsFkFxrKVPizbzM145eVEzFEahZAO8TqGMestK7xzPZAkiS3Lg7ZBaf4RfrAewZBli6aUP9pqzgZDZD"
 
 
 def eprint(*args, **kwargs):
@@ -26,12 +27,23 @@ app.secret_key = config["session-secret"]
 
 @app.route("/fb_callback", methods=["GET", "POST"])
 def callback():
-    data = request.form
+    if request.method == "POST":
+        return post_callback()
+    elif request.method == "GET":
+        return get_callback()
 
-    if data["hub.mode"] == "subscribe" and data["hub.verify_token"] == app_key:
-        return data["hub.challenge"], status.HTTP_200
+
+def get_callback():
+    data = request.args
+
+    if data.get("hub.mode") == "subscribe" and data.get("hub.verify_token") == app_key:
+        return data.get("hub.challenge"), status.HTTP_200
     else:
         return "", status.HTTP_403_FORBIDDEN
+
+
+def post_callback():
+    eprint("hot post callback")
 
 if __name__ == "__main__":
     run_simple(config["website"], config["port"], app,
