57c0fca5fe6d8d4c12fdbf40e1988a7df9242fa4
719Ben <ben.williams@colorado.edu>
2016-08-06 22:33:21 -0700

    Added basic messages

--- main.py
+++ main.py
@@ -43,7 +43,64 @@ def get_callback():
 
 
 def post_callback():
-    eprint("hot post callback")
+    data = request.get_json()
+    eprint(data)
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    # the facebook ID of the person sending you the message
+                    sender_id = messaging_event["sender"]["id"]
+                    # the recipient's ID, which should be your page's facebook
+                    # ID
+                    recipient_id = messaging_event["recipient"]["id"]
+                    message_text = messaging_event["message"][
+                        "text"]  # the message's text
+
+                    send_message(sender_id, "got it, thanks!")
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                # user clicked/tapped "postback" button in earlier message
+                if messaging_event.get("postback"):
+                    pass
+
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(
+        recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": page_access_token
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        eprint(r.status_code)
+        eprint(r.text)
+
 
 if __name__ == "__main__":
     run_simple(config["website"], config["port"], app,
