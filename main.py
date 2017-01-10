5c219b7bb85d5182b4720aa5aaae9ff29395e760
719Ben <ben.williams@colorado.edu>
2017-01-10 12:37:37 -0700

    Added local site to config, used for ngrock testing

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ def bye():
 
 
 if __name__ == "__main__":
-    run_simple("localhost", utils.config["port"], app,
+    run_simple(utils.config["local_website"], utils.config["port"], app,
                use_reloader=True, use_debugger=True, use_evalex=True)
